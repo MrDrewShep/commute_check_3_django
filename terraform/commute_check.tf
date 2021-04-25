74f1ca0adcab44eb641d78c21066f65c405c28e9
user name <mrdrewshep@users.noreply.github.com>
2021-04-25 11:47:40 -0400

    Setup security group, keypair, and ec2

--- terraform/commute_check.tf
+++ terraform/commute_check.tf
@@ -1,6 +1,10 @@
 resource "aws_instance" "commute_check" {
   ami           = var.ami
   instance_type = "t2.micro"
+
+  key_name = "flask_ec2" #TODO replace
+  security_groups = [module.ssh_security_group.this_security_group_name]
+
   tags = {
     Name = "commute-check-v3-app"
   }
--- terraform/network.tf
+++ terraform/network.tf
@@ -1,4 +1,24 @@
+resource "aws_default_vpc" "default" {}
+
 resource "aws_eip_association" "eip_association" {
   instance_id = aws_instance.commute_check.id
   public_ip   = var.eip
+}
+
+# resource "security_group" "nginx_container_access" {
+#   name = "nginx_container_access"
+#   vpc_id = data.aws_default_vpc.default.id
+# }
+
+# TODO how do you have multiple modules, if you can't "name" them?
+module "ssh_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/ssh"
+  version = "3.18.0"
+
+  name        = "nginx_ssh_access"
+  description = "SSH access into the nginx container"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  auto_ingress_rules  = ["ssh-tcp"]
 }
